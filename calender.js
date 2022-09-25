ab8fb666d42470a80e78d3a8bb71ab2af5f1f222
L.Robinson <110437490+codeact1@users.noreply.github.com>
2022-09-24 20:49:21 -0500

    assignment 3
    Allows the user to create Tasks
    - Create the task object
    - Displays the task on the screen
    - Clears the form after creating the task

--- calender.js
+++ calender.js
@@ -1,53 +1,87 @@
-var importantIcon = "fas fa-exclamation"
-var nonImportantIcon = "far fa-star"
+var importantIcon = "fas fa-exclamation";
+var nonImportantIcon = "far fa-star";
 var isImportant = false;
 var isVisible = true;
 //$(".info").hide();
 
-function saveTask(){
-    console.log("Button Clicked");
+function saveTask() {
+ 
+
+  let title = $("#txtTitle").val();
+  let description = $("#txtDescription").val();
+  let tag = $("#txtTag").val();
+  let dueDate = $("#txtDueDate").val();
+  let category = $("#txtCategory").val();
+  let color = $("#txtColor").val();
+
+  let task = new Task(title, description, tag, dueDate, category, color);
+  displayTask(task);
+  clearForm();
 }
 
-function changeIcon(){
-    if (isImportant == false) {
-        //change to important
-    console.log("Icon Clicked!")
+function displayTask(task) {
+  
+  let syntax = `<div class="task">
+
+    <div class="title-des">
+        <h4>${task.title}</h4>
+        <p>${task.description}</p>
+     </div>
+
+     <label class="date">${task.dueDate}</label>
+
+     <div class="cat-tag">
+        <label>${task.category}</label>
+        <label>${task.tag}</label>
+     </div>
+  
+  </div>`;
+
+  $("#taskList").append(syntax);
+}
+function clearForm() {
+    $("#txtTitle").val("");
+    $("#txtDescription").val("");
+    $("#txtTag").val("");
+    $("#txtDueDate").val("");
+    $("#txtCategory").val("");
+    $("#txtColor").val("");
+}
+
+
+function changeIcon() {
+  if (isImportant == false) {
+    //change to important
+    
 
     $("#iImportant").removeClass(nonImportantIcon).addClass(importantIcon);
     isImportant = true;
-    }
-    else{
-        //change to non important
-        $("#iImportant").removeClass(importantIcon).addClass(nonImportantIcon);
-        isImportant = false; 
-    }
+  } else {
+    //change to non important
+    $("#iImportant").removeClass(importantIcon).addClass(nonImportantIcon);
+    isImportant = false;
+  }
 }
 
-function showForm(){
-    if(isVisible){
-        $(".info").hide();
-        isVisible = false;
-    }
-    else{
-        $(".info").show();
-        isVisible = true;
-    }
+function showForm() {
+  if (isVisible) {
+    $(".info").hide();
+    isVisible = false;
+  } else {
+    $(".info").show();
+    isVisible = true;
+  }
 }
 
-function init(){
-    console.log("Task Manager");
+function init() {
+  
+  // load prev data
 
-    // load prev data
+  //catch events
 
-    //catch events
-
-
-$("#btnSave").click(saveTask);
-$("#iImportant").click(changeIcon);
-$("#showForm").click(showForm);
-
-
+  $("#btnSave").click(saveTask);
+  $("#iImportant").click(changeIcon);
+  $("#showForm").click(showForm);
 }
-
 
 window.onload = init;
---
+++ task.js
@@ -0,0 +1,12 @@
+
+class Task {
+
+    constructor(title, desc, tag, dueDate, cat, color) {
+        this.title = title;
+        this.description = desc;
+        this.tag = tag;
+        this.dueDate = dueDate;
+        this.category = cat;
+        this.color = color;
+    }
+}
